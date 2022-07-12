0c6db8fcb79af0ccaef2da6cbe53312fba46c1bd
Molls13 <96827554+Molls13@users.noreply.github.com>
2022-07-12 13:31:18 +0200

    attempt to consume serverless function at build

--- pages/products/[name].js
+++ pages/products/[name].js
@@ -2,25 +2,35 @@ import { fetchEntries } from "../../contentful"
 
 const Product = ({ product }) => {
   return (
-    <p>{product.name}</p>
+    <>
+      <p>{product.name}</p>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <p>{product.quantity}</p>
+    </>
   )
 }
 
 export const getStaticProps = async (context) => {
-    const products = await fetchEntries()
+  const products = await fetchEntries()
+  const product = products.find(product => product.name === decodeURIComponent(context.params.name))
+  const quantity = await fetch(`/.netlify/functions/getQuantity?id=${product.id}`)
   return {
     props: {
-        product: products.find(product => product.name === decodeURIComponent(context.params.name))
+      product: {
+        ...product,
+        quantity
+      }
     }
   }
 }
 
-export const getStaticPaths = async() => {
-    const products = await fetchEntries()
-    const paths = products.map(product => ({
-        params: {name: product.name},
-    }));
-    return {paths, fallback: false}
+export const getStaticPaths = async () => {
+  const products = await fetchEntries()
+  const paths = products.map(product => ({
+    params: { name: product.name },
+  }));
+  return { paths, fallback: false }
 }
 
 export default Product
