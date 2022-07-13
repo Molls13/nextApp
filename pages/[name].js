ecc2df7a9a804eb46d386a384ccaf2368b1d92f2
Molls13 <96827554+Molls13@users.noreply.github.com>
2022-07-13 11:16:26 +0200

    try again

---
+++ pages/[name].js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react"
+import { fetchEntries } from "../contentful"
+
+const Product = ({ product }) => {
+  const [quantity, setQuantity] = useState(null)
+  useEffect(() => {
+    const abortController = new AbortController();
+    const getAndSet = async () => {
+      try {
+        const response = await fetch(
+          `https://promise-all-266538.netlify.app/.netlify/functions/getQuantity?id=${product.id}`,
+          {
+            signal: abortController.signal
+          }
+        );
+        const { quantity } = await response.json();
+        setQuantity(quantity);
+      } catch(e) {
+        console.log(e)
+      }
+    };
+    getAndSet();
+    return () => abortController.abort()
+  }, [])
+  const handleBuy = async () => {
+    setQuantity(null);
+    const response = await fetch(`/api/products/${product.id}`, { method: 'POST' });
+    const { newQuantity } = await response.json()
+    // setQuantity(prev => prev -1)
+    setQuantity(newQuantity)
+  }
+  return (
+    <>
+      <p>{product.name}</p>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <p>{quantity === null ? "Loading..." : quantity}</p>
+      <button onClick={handleBuy} disabled={!quantity}>Buy 1</button>
+    </>
+  )
+}
+
+export const getStaticProps = async (context) => {
+  const products = await fetchEntries()
+  const product = products.find(product => product.name === decodeURIComponent(context.params.name))
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const products = await fetchEntries()
+  const paths = products.map(product => ({
+    params: { name: product.name },
+  }));
+  return { paths, fallback: false }
+}
+
+export default Product
